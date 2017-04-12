e956cab463487fdcb393f4c02723dcbd17dae935
juliakimchung <julhkm@gmail.com>
2017-04-12 10:31:03 -0500

    indentation

--- app/controller/ResDetailCtrl.js
+++ app/controller/ResDetailCtrl.js
@@ -1,46 +1,46 @@
 "use strict";
 app.controller("ResDetailCtrl", 
 function($http, RootFactory, $location, $scope, $timeout, $routeParams){
-		$scope.reservations = [];
-		$scope.rooms = [];
-		$scope.payment = [];
-		RootFactory.getApiRoot()
-		.then((rootes)=>{
+	$scope.reservations = [];
+	$scope.rooms = [];
+	$scope.payment = [];
+	RootFactory.getApiRoot()
+	.then((rootes)=>{
+		$http({
+			url: `${rootes.reservation}${$routeParams.reservationId}/`,
+			method: 'GET',
+			headers: {
+				'Authorization': `Token ${RootFactory.getToken()}`
+			}
+		})
+		.then((items) => {
+			console.log("items from ResDetailCtrl", items);
+			$scope.reservations = items.data;
+			$http({
+				url: `${rootes.room}${$scope.reservations.room}/`,
+				method: "GET",
+				headers: {
+					'Authorization': `Token ${RootFactory.getToken()}`
+				}
+			})
+			.then((rooms)=> {
+				$scope.rooms = rooms.data;
+				console.log("rooms from ResDetailCtrl", rooms );
 				$http({
-					url: `${rootes.reservation}${$routeParams.reservationId}/`,
-					method: 'GET',
+					url: `${rootes.payment_type}${$scope.reservations.payment}/`,
+					method:"GET",
 					headers: {
 						'Authorization': `Token ${RootFactory.getToken()}`
 					}
 				})
-				.then((items) => {
-					console.log("items from ResDetailCtrl", items);
-					$scope.reservations = items.data;
-					$http({
-						url: `${rootes.room}${$scope.reservations.room}/`,
-						method: "GET",
-						headers: {
-							'Authorization': `Token ${RootFactory.getToken()}`
-						}
-					})
-					.then((rooms)=> {
-						$scope.rooms = rooms.data;
-						console.log("rooms from ResDetailCtrl", rooms );
-						$http({
-							url: `${rootes.payment_type}${$scope.reservations.payment}/`,
-							method:"GET",
-							headers: {
-								'Authorization': `Token ${RootFactory.getToken()}`
-							}
-						})
-						.then((payment)=>{
-							$scope.payment = payment.data;
+				.then((payment)=>{
+					$scope.payment = payment.data;
 
-							console.log("payment from ResDetailCtrl", payment );
-						});
-					});
+					console.log("payment from ResDetailCtrl", payment );
 				});
+			});
 		});
+	});
 });
 	
 
