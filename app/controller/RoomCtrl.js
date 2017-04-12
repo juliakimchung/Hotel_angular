123954d2cf01721b0daa81579beebb9960f80d20
juliakimchung <julhkm@gmail.com>
2017-04-12 10:27:02 -0500

    indentation correction

--- app/controller/RoomCtrl.js
+++ app/controller/RoomCtrl.js
@@ -1,19 +1,19 @@
 'use strict';
 app.controller("RoomCtrl",
 function($http, RootFactory, $location, $scope){
-		$scope.rooms = [];
-		RootFactory.getApiRoot()
-		.then((rootes)=>{
-				$http({
-					url: `${rootes.room}`,
-					method: 'GET',
-					headers: {
-						'Authorization': `Token ${RootFactory.getToken()}`
-					}
-				})
-				.then((items) => {
-					console.log("items from RoomCtrl", items);
-					$scope.rooms = items.data.results;
-				});
+	$scope.rooms = [];
+	RootFactory.getApiRoot()
+	.then((rootes)=>{
+			$http({
+				url: `${rootes.room}`,
+				method: 'GET',
+				headers: {
+					'Authorization': `Token ${RootFactory.getToken()}`
+				}
+			})
+			.then((items) => {
+				console.log("items from RoomCtrl", items);
+				$scope.rooms = items.data.results;
 			});
+	});
 });
